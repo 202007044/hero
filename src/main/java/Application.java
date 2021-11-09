18be409540e99a2770d2d8c623d33b0d83804bb5
you@2.com <you@2.com>
2021-11-09 14:07:08 +0000

    6

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,29 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.terminal.*;
+import com.googlecode.lanterna.screen.*;
+
+import java.io.IOException;
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        Terminal terminal = new;
+        System.out.println("Hello world");
+        try {
+            TerminalSize terminalSize = new TerminalSize(40, 20);
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+            screen.clear();
+            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+            screen.refresh();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
